03dfd51d7b4fccbfa2d28ac9e2a6b68ea06c7c71
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2024-09-01 18:05:02 +0500

    Added index, config, login and register pages

--- index.php
+++ index.php
@@ -1,29 +1,32 @@
+<?php
+// Allow the config
+define('__CONFIG__', true);
+require_once "include/config.php"
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="description" content="Free Web tutorials">
-        <meta name="keywords" content="HTML, CSS, JavaScript">
-        <meta name="author" content="John Doe">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login Page</title>
-        <link rel="stylesheet" href="styles/base.css">
-        <link rel="stylesheet" href="styles/themes/light_mode.css">
-    </head>
-    <body>
-        <div class="header light-mode">
-            <h1 class="heading">Login Page</h1>
-            <form method="POST" class="login-form js-login">
-                <label for="username">Username</label>
-                <input type="email" name="username" required="required">
-                <label for="password">Password</label>
-                <input type="password" name="password" required="required">
-                <button type="submit">Log In</button>
-            </form>
-            <div class="register-div">
-                <label for="register">New here?</label>
-                <button name="register">Register</button>
-            </div>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="John Doe">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="styles/base.css">
+    <link rel="stylesheet" href="styles/themes/light_mode.css">
+</head>
+
+<body>
+    <div class="header light-mode">
+        <a href="/"><h1 class="heading">Php Login System</h1></a>
+        <div class="login-div">
+            <a href="/login.php"><button name="login">Log In</button></a>
         </div>
-    </body>
+        <div class="register-div">
+            <a href="/register.php"><button name="register">Register</button></a>
+        </div>
+    </div>
+</body>
+
 </html>
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+    // Allow the config
+    define('__CONFIG__', true);
+    require_once "include/config.php"
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Free Web tutorials">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="author" content="John Doe">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login Page</title>
+        <link rel="stylesheet" href="styles/base.css">
+        <link rel="stylesheet" href="styles/themes/light_mode.css">
+    </head>
+    <body>
+        <div class="header light-mode">
+            <a href="/"><h1 class="heading">Php Login System</h1></a>
+            <form method="POST" class="login-form js-login">
+                <label for="username">Username</label>
+                <input type="email" name="username" required="required">
+                <label for="password">Password</label>
+                <input type="password" name="password" required="required">
+                <button type="submit">Log In</button>
+            </form>
+            <div class="register-div">
+                <label for="register">New here?</label>
+                <a href="/register.php"><button name="register">Register</button></a>
+            </div>
+        </div>
+    </body>
+</html>
